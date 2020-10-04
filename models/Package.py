1d4025af9033804a37d8d942500dc1e000cf0eaa
Joseph Surla <josephsurla@gmail.com>
2020-10-04 12:58:05 +0900

    Create Package class

---
+++ models/Package.py
@@ -0,0 +1,13 @@
+class Package:
+
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, notes = 'none', status = 'At hub'):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.deadline = deadline
+        self.weight = weight
+        self.notes = notes
+        self.status = status
+
