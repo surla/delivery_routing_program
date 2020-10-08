58c8b8f1b14876da0bce18450f45a377ef7f4fa5
Joseph Surla <josephsurla@gmail.com>
2020-10-08 17:58:06 +0900

    Add utf-8-sig encoding

--- utils/csv_reader.py
+++ utils/csv_reader.py
@@ -3,7 +3,7 @@ import csv
 
 def get_data(filename):
     data = []
-    with open(filename) as csvfile:
+    with open(filename, encoding="utf-8-sig") as csvfile:
         read_data = csv.reader(csvfile, delimiter=',')
         for row in read_data:
             data.append(row)
