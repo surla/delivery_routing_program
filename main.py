ae182c4e52e84b20631e6ea0396b606b0cb44382
Joseph Surla <josephsurla@gmail.com>
2020-10-10 17:26:27 +0900

    Create method to sort packages by distances from HUB

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from models.Truck import Truck
 from utils.hash_table import HashTable
 
 
-packages = HashTable()  # Hash table of all packages in package.csv
-distances = HashTable()  # Hash table of all packages in distance.csv
-destinations = []  # List of all addresses of delivery destinations
+all_packages = HashTable()  # Hash table of all packages in package.csv
+all_distances = HashTable()  # Hash table of all packages in distance.csv
+all_destinations = []  # List of all addresses of delivery destinations
 
-early_deliveries = [1, 6, 13, 14, 15, 16, 20, 25, 29, 30, 31, 34, 37, 40]
+early_deliveries = [1, 2, 4, 13, 14, 15, 16, 19, 20, 21, 29, 30, 31, 34, 37, 40]
+truck_2 =[3, 5, 6, 7, 8, 9, 10, 11, 12, 17, 18, 25, 28, 32, 36, 38]
+left_over = [22, 23, 24, 26, 27, 33, 35, 39]
 
 
 def get_packages():
@@ -17,7 +19,7 @@ def get_packages():
     # Loads all instances of package into global variable packages
     for item in data:
         package = Package(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7])
-        packages.add(item[0], package)  # Key is package_id
+        all_packages.add(item[0], package)  # Key is package_id
 
 
 def get_distances():
@@ -25,52 +27,69 @@ def get_distances():
 
     for item in data:
         address = item[0]  # Variable for address
-        destinations.append(address)  # Used to look up distances during delivery.
-        distances.add(address, item)  # Adds address as key and distances as value
+        all_destinations.append(address)  # Used to look up distances during delivery.
+        all_distances.add(address, item)  # Adds address as key and distances as value
 
     # print(distances['6351 South 900 East'][8])  # this will get distances between locations
     # print(destinations)
 
 
+#  Bubble Sort as greedy algorithm to get packages in order from closest to farthest
+def sort_packages(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n-1):
+            if float(all_distances[all_packages.get(arr[j]).address][1]) > float(all_distances[all_packages.get(arr[j+1]).address][1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+
 # Method will load packages and get delivery distances
-def delivery(group):
-    num = [1,2,3,4,5,6]
-      # sets current location to HUB
-    truck = Truck()
+def delivery(truck, group):
+    # sets current location to HUB
     location = truck.location
 
-    for i in group:
-        truck.load_package(packages.get(i))
+    sorted = sort_packages(group)
+
+    for i in sorted:
+        truck.load_package(all_packages.get(i))
+        print(all_distances[all_packages.get(i).address][1])
 
     # Get index of destination
     for item in truck.load:
 
         address = item.address
-        delivery_address = destinations.index(address)
-
+        delivery_address = all_destinations.index(address)
         # Change truck distance and package status for delivery
-        truck.distance += float(distances[location][delivery_address])  # Updates truck distance after delivery
+        truck.distance += float(all_distances[location][delivery_address])  # Updates truck distance after delivery
         item.status = 'Delivered'  # Set status to delivered after delivery
-        print(item.status)
-        print(distances[location][delivery_address])  # Gets distances between current location and delivery address
         location = address  # Sets location to address destination
 
     truck.load.clear()  # Truck finishes delivery clear load
     # Truck goes to 'HUB' after last package delivery
-    truck.distance += float(distances[location][1])
+    truck.distance += float(all_distances[location][1])
     truck.location = 'HUB'
-
-    print(len(truck.load))
-    print(truck.location)
-    print(truck.distance)
 
 
 def start():
     print("-------- Welcome to Package Delivery System --------")
 
+    truck1 = Truck()
+    truck2 = Truck()
+
     get_packages()
     get_distances()
-    delivery(early_deliveries)
+    delivery(truck1, early_deliveries)
+    delivery(truck2, truck_2)
+    delivery(truck1, left_over)
+
+    print("Truck 1 distance: " + str(truck1.distance))
+    print("Truck 2 distance: " + str(truck2.distance))
+    print(truck1.distance + truck2.distance)
+
+
 
 
 def print_hi(name):
